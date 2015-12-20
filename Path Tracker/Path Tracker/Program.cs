700f6e85d6030aac54082ac30bc4fb6a254c1c23
SuperNova911 <suwhan77@naver.com>
2015-12-20 18:24:35 +0900

    Add disable while use orbwalk

--- Path Tracker/Path Tracker/Program.cs
+++ Path Tracker/Path Tracker/Program.cs
@@ -14,6 +14,8 @@ namespace Path_Tracker
     {
         public static Menu Menu;
 
+        public static float ETA = 0;
+
         static void Main(string[] args)
         {
             Loading.OnLoadingComplete += Loading_OnLoadingComplete;
@@ -26,10 +28,16 @@ namespace Path_Tracker
             Menu.Add("me", new CheckBox("My Path", false));
             Menu.Add("ally", new CheckBox("Ally Path", false));
             Menu.Add("enemy", new CheckBox("Enemy Path", true));
-            Menu.AddSeparator();
+            Menu.AddLabel("Misc");
             Menu.Add("eta", new CheckBox("Estimated time of arrival", false));
             Menu.Add("name", new CheckBox("Champion Name", true));
             Menu.Add("thick", new Slider("Line Thickness", 2, 1, 5));
+            Menu.AddGroupLabel("Disable while use orbwalk");
+            Menu.Add("combo", new CheckBox("Combo", true));
+            Menu.Add("harass", new CheckBox("Harass", true));
+            Menu.Add("laneclear", new CheckBox("LaneClear", true));
+            Menu.Add("lasthit", new CheckBox("LastHit", true));
+            Menu.Add("flee", new CheckBox("Flee", false));
 
             Drawing.OnDraw += Drawing_OnDraw;
         }
@@ -40,7 +48,7 @@ namespace Path_Tracker
             var Name = Menu["name"].Cast<CheckBox>().CurrentValue;
             var Thickness = Menu["thick"].Cast<Slider>().CurrentValue;
 
-            if (Menu["me"].Cast<CheckBox>().CurrentValue)
+            if (Menu["me"].Cast<CheckBox>().CurrentValue && Enable())
             {
                 DrawPath(Player.Instance, Thickness, Color.LawnGreen);
 
@@ -113,8 +121,23 @@ namespace Path_Tracker
                     Distance += unit.Path[i - 1].Distance(unit.Path[i]);
                 }
             }
-
+            
             return (Distance / unit.MoveSpeed).ToString("F1");
+        }
+
+        public static bool Enable()
+        {
+            if (Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Combo && Menu["combo"].Cast<CheckBox>().CurrentValue)
+                return false;
+            if (Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Harass && Menu["harass"].Cast<CheckBox>().CurrentValue)
+                return false;
+            if ((Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.LaneClear || Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.JungleClear) && Menu["laneclear"].Cast<CheckBox>().CurrentValue)
+                return false;
+            if (Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.LastHit && Menu["lasthit"].Cast<CheckBox>().CurrentValue)
+                return false;
+            if (Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Flee && Menu["flee"].Cast<CheckBox>().CurrentValue)
+                return false;
+            return true;
         }
     }
 }
